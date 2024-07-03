3e49be8b72cad42646316be2dc0058518e3d71c3
StefanoWind <stefano.letizia@nrel.gov>
2024-07-03 15:18:44 -0600

    fixed error on pressure

--- tropoe_inputs.py
+++ tropoe_inputs.py
@@ -91,7 +91,7 @@ logger.error(result.stderr)
 
 #remove atmospheric pressure that causes error
 if len(glob.glob(os.path.join(cd,'data',channel_irs,'nfc','*'+date+'*cdf')))==1:
-    f_ch1=glob.glob(os.path.join(cd,'data',channel_irs,'*'+date+'*cha*cdf'))[0]
+    f_ch1=glob.glob(glob.glob(os.path.join(cd,'data',channel_irs,'nfc','*'+date+'*cdf')))[0]
     Data_ch1=xr.open_dataset(f_ch1).sortby('time')
     Data_ch1=Data_ch1.rename_vars({'atmosphericPressure':'deprecated_atmosphericPressure'})
     Data_ch1.to_netcdf(f_ch1)
