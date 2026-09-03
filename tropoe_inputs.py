0987c4bc6d5dcaa03e2bbc71db930db96e672d73
StefanoWind <ltzsfn@gmail.com>
2026-09-03 10:57:26 -0600

    lidar data averaged by time

--- tropoe_inputs.py
+++ tropoe_inputs.py
@@ -72,9 +72,9 @@ else:
     sumassistdir=os.path.join(tmpdir,'sum')
     nfchassistdir=os.path.join(tmpdir,'nfc')
 
-# #clear old temp files
-# if os.path.exists(tmpdir):
-#     shutil.rmtree(tmpdir)
+#clear old temp files
+if os.path.exists(tmpdir):
+    shutil.rmtree(tmpdir)
     
 os.makedirs(tmpdir,exist_ok=True)
 
@@ -105,20 +105,20 @@ if config['override_hatch']:
     trp.overrride_hatch_flag(glob.glob(os.path.join(chassistdir,'*cdf')),logger=logger)
     trp.overrride_hatch_flag(glob.glob(os.path.join(sumassistdir,'*cdf')),logger=logger)
     
-# #pca filter
-# if config['N_days_nfc'][site]>1:
-#     logger.info('Running PCA filter')
-#     command=config['path_python']+f' {os.path.join(cd,"utils","run_irs_nf.py")} --create {sdate} {edate} {chassistdir} {sumassistdir} {nfchassistdir} "assist"'
-#     result = subprocess.run(command, shell=True, text=True,capture_output=True)
-#     logger.info(result.stdout)
-#     logger.error(result.stderr)
+#pca filter
+if config['N_days_nfc'][site]>1:
+    logger.info('Running PCA filter')
+    command=config['path_python']+f' {os.path.join(cd,"utils","run_irs_nf.py")} --create {sdate} {edate} {chassistdir} {sumassistdir} {nfchassistdir} "assist"'
+    result = subprocess.run(command, shell=True, text=True,capture_output=True)
+    logger.info(result.stdout)
+    logger.error(result.stderr)
     
-#     command=config['path_python']+f' {os.path.join(cd,"utils","run_irs_nf.py")} --apply {sdate} {edate} {chassistdir} {sumassistdir} {nfchassistdir} "assist"'
-#     result = subprocess.run(command, shell=True, text=True,capture_output=True)
-#     logger.info(result.stdout)
-#     logger.error(result.stderr)
-# else:
-#     logger.info('PCA filter skipped')
+    command=config['path_python']+f' {os.path.join(cd,"utils","run_irs_nf.py")} --apply {sdate} {edate} {chassistdir} {sumassistdir} {nfchassistdir} "assist"'
+    result = subprocess.run(command, shell=True, text=True,capture_output=True)
+    logger.info(result.stdout)
+    logger.error(result.stderr)
+else:
+    logger.info('PCA filter skipped')
     
 #remove atmospheric pressure that causes error
 if len(glob.glob(os.path.join(nfchassistdir,'*'+date+'*cdf')))==1:
